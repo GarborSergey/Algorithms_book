2b36f0f7d02e94971bd868e69f8a6cd1dc0c1c2e
GarborSergey <garborfersru@gmail.com>
2022-02-12 14:38:07 +0300

    Бинарный поиск

---
+++ binary_search.py
@@ -0,0 +1,27 @@
+# Бинарный поиск
+def binary_search(list: list, item):
+    # В этих переменных хранятся границы той части списка
+    # в которой происходит поиск
+    low = 0
+    high = len(list) - 1
+    i = 0
+
+    while low <= high:  # Пока граница не сократиться до одного элемента
+        i += 1
+        print('attempt ', i)
+        mid = int((low + high) / 2)  # Проверяем средний элемент
+        guess = list[mid]
+        if guess == item:  # Значение найдено
+            return mid + 1
+        if guess > item:  # Много
+            high = mid - 1
+        else:  # Мало
+            low = mid + 1
+    return None  # Значение не существует
+
+
+my_list = range(120)
+x = binary_search(my_list, 59)
+print(x)
+print(*my_list)
+print(len(my_list))
