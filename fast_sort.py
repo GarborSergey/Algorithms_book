c4ee365c81c363a4b89a9d2f9f1a4c1c12b21e3a
GarborSergey <garborfersru@gmail.com>
2022-02-14 09:36:57 +0300

    Быстрая сортировка рекурсией

---
+++ fast_sort.py
@@ -0,0 +1,11 @@
+# Быстрая сортировка рекурсией
+def quick_sort(arr: list):
+    if len(arr) < 2:
+        return arr
+    else:
+        pivot = arr[0]
+        less = [i for i in arr[1:] if i <= pivot]
+        greater = [i for i in arr[1:] if i > pivot]
+        return quick_sort(less) + [pivot] + quick_sort(greater)
+
+print(quick_sort([1, 10, 10, 4, 5, 55556, 2, 2, 43, 23]))
