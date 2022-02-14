533a36f8f95ae87795a133aca92f134dfd83cb41
GarborSergey <garborfersru@gmail.com>
2022-02-14 09:15:33 +0300

    Рекурсия

--- recursion.py
+++ recursion.py
@@ -18,7 +18,7 @@ def count_down1(i):
         count_down1(i - 1)
 
 
-count_down1(5)
+'''count_down1(5)'''
 
 
 # Рекурсивная функция по вычислению факториала
@@ -29,4 +29,42 @@ def fact(x):
         return x * fact(x-1)
 
 
-print(fact(5))
+'''print(fact(5))'''
+
+
+# Рекурсивная функция по расчету суммы элементов списка
+def sum_arr(arr: list):
+    if len(arr) == 0:
+        return 0
+    else:
+        return arr.pop(0) + sum_arr(arr)
+
+
+print(sum_arr([1, 10, 10, 213, 13]))
+
+
+# функция по подсчету элементов в списке
+def len_arr(arr: list):
+    if len(arr) == 0:
+        return 0
+    else:
+        arr.pop(-1)
+        return 1 + len_arr(arr)
+
+print(len_arr([1, 10, 10, 4, 5, 5, 2, 2, 43, 23]))
+
+
+# Наибольшее число в списке
+def max_arr(arr: list):
+    if len(arr) == 1:
+        return arr[0]
+    else:
+        if arr[0] > arr[1]:
+            arr.pop(1)
+            return max_arr(arr)
+        else:
+            arr.pop(0)
+            return max_arr(arr)
+
+print(max_arr([1, 10, 10, 4, 5, 55556, 2, 2, 43, 23]))
+
--- test.py
+++ test.py
@@ -1,3 +1,13 @@
-a = 'B'
-b = 'C'
-print(a < b)
+def sum_arr(arr: list):
+    if len(arr) == 0:
+        return 0
+    else:
+        x = arr[0]
+        arr.pop(0)
+        return x + sum_arr(arr)
+
+
+x = [1, 10, 10]
+print(sum_arr(x))
+y = []
+
