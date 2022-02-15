ed6e56411045435599b8235a32122b1591f45c59
GarborSergey <garborfersru@gmail.com>
2022-02-15 10:14:43 +0300

    Алгоритм Дейкстры

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-def sum_arr(arr: list):
-    if len(arr) == 0:
-        return 0
-    else:
-        x = arr[0]
-        arr.pop(0)
-        return x + sum_arr(arr)
-
-
-x = [1, 10, 10]
-print(sum_arr(x))
-y = []
 
